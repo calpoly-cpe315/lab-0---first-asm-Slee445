6a46fe00995deac27281e60ee8093b419ec4b3ee
Slee445 <76962168+Slee445@users.noreply.github.com>
2022-02-04 12:41:47 -0800

    Add files via upload

--- eggnog.s
+++ eggnog.s
@@ -1,15 +1,26 @@
-    .global	main
+.global	main
 main:
     // pre-increment the stack point to create space for two 8-byte registers
     // link register (x30), and frame pointer (x29), and store them.
 	stp	x29, x30, [sp, -16]!
 
     // Load the argument and perform the call. Like 'printf("...")' in C.
-	ldr	x0, =message
-	bl	printf
+	ldr	x0, =message1
+        bl      printf
 
     // initialize the return value in the return register
 	mov	x0, 0
+
+    //Printing out the other two messages
+        ldr     x0, =message2
+        bl      printf
+        mov     x0, 0
+  
+
+        ldr     x0, =message3
+        bl      printf
+        mov     x0, 0
+
 
     // restore the registers and post-decrement 
     // the stack pointer for consistency
@@ -17,6 +28,10 @@ main:
 
     // return from the call
 	ret
-
-message:
-	.asciz "Hello, world!\n"
+        
+message1:
+	.asciz "White and creamy silk\n"
+message2:
+        .asciz "teasing tantalizing milk\n"
+message3:
+        .asciz "how I crave thy eggnog\n"
